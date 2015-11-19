aafd99bb28036c62e33351bae761bb3f6875926f
cedreg <cedric.regenass@hf.ict.info>
2015-11-19 20:04:07 +0100

    v0.2
    Next

--- aufgabe_14-2.cpp
+++ aufgabe_14-2.cpp
@@ -13,6 +13,13 @@ int main()
     cout << "Bitte ein Jahr zwischen 1900 und 2099 eingeben:" << endl;
     cin >> jahr;
 	
+    //überprüfen ob jahr im richtigen zeitabschnitt
+    if (jahr >= 1900 && jahr <= 2099){
+        //ausführung programm
 
+        ende = algOtag(jahr); //hajr an function schicken und ergebniss "ende" zuweisen
+	}
+
+	
     return 0;
 }
