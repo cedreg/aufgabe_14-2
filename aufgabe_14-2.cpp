340616cc2630c651506abe4dfefca4f616458e4c
cedreg <cedric.regenass@hf.ict.info>
2015-11-19 20:04:42 +0100

    v0.3
    test

--- aufgabe_14-2.cpp
+++ aufgabe_14-2.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int algOtag(int y)
+{
+    int n(0), a(0), b(0), m(0), q(0), w(0), k(25);
+
+    n = y - 1900;
+    a = n % 19;
+    b = ((7 * a) + 1) / 19;
+    m = (((11 * a) + 4) - b) % 29;
+    q = n / 4;
+    w = (((n + q) + 31) -m) % 7;
+    k = (k -m) -w;
+
+    return k;
+}
 
 int main()
 {
