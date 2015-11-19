e75d3f58576b249612cca3eaf924a1b1155a38a7
cedreg <cedric.regenass@hf.ict.info>
2015-11-19 20:02:21 +0100

    V0.1
    next

--- aufgabe_14-2.cpp
+++ aufgabe_14-2.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 int main()
 {
+    //Definitionen
+    int jahr(0), ende(0);
+
+    //einlesen von dem Jahr
+    cout << "Ostern herausfinden!" << endl;
+    cout << "Bitte ein Jahr zwischen 1900 und 2099 eingeben:" << endl;
+    cin >> jahr;
+	
+
     return 0;
 }
